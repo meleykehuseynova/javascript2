34efd66a96508cfd5234a9d1012d860f8db433ab
meleykehuseynova <161501438+meleykehuseynova@users.noreply.github.com>
2024-03-27 00:33:58 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,104 @@
+//Daxil olunan ededin sade ve murekkeb oldugunu tapn
+
+// let a =+ prompt("ededi daxil edin:");
+// let b = 0;
+// for(i=1;i<=a;i++){
+//     if(a%i===0){
+//         b+=1;
+//     }
+//     else{
+//         continue;
+//     }
+// }
+// if(a===1){
+//     console.log(`ne sadedir ne murekkeb`);
+// }
+// else if(b<1){
+//     console.log(`daxil edilen eded duzgun deyil`);
+// }
+// else if(b>2){
+//     console.log(`murekkebdir`);
+// }
+// else{
+//     console.log(`sadedir`);
+// }
+
+
+              // Daxil edilen iki eded arasinda sade ededi gosterin
+    //   let a= +prompt("birinci ededi daxil edin:");
+    //   let b= +prompt("ikinci ededi daxil edin:");
+    //   let bolen=0;
+    //   for(let i=a;i<=b;i++){
+    //     bolen=0;
+    //     for(let j=1;j<=i;j++){
+    //         if(i%j===0){
+    //             bolen+=1;
+    //         }
+    //     }
+    //     if(bolen<=2){
+    //         console.log(`${i}`);
+    //     }
+    // }
+
+
+    // daxil olunan ededin mukemmel olub olmadigini yoxlayin
+// let a = +prompt("ededi daxil edin:");
+// let sum = 1;
+// for (let i = 2; i <= Math.sqrt(a); i++) {
+//     if (a % i === 0) {
+//         sum += i;
+//         if (i !== a / i) {
+//             sum += a / i; 
+//         }
+//     }
+// }
+// if (sum === a) {
+//     console.log(`${a}, mukemmel ededdir`);
+// } else {
+//     console.log(`${a} mukemmel eded deyil`);
+// }
+
+
+     //daxil edilen 2 eded arasinda mukemmel ededler
+
+    //    let a = +prompt(" Birinci ededi daxil edin:");
+    //    let b = +prompt("Ikinci ededi daxil edin:");
+      
+    //    console.log( `${a} ${b} arasindaki mukemmel ededler` );
+       
+    //    for (let number = a; number <= b; number++) {
+    //     let sum = 1;
+    //        for (let i = 2; i <= Math.sqrt(number); i++) {
+    //            if (number % i === 0) {
+    //                sum += i;
+    //                if (i !== number / i) {
+    //                    sum += number / i; 
+    //                }
+    //            }
+    //        }
+    //        if (sum === number) {
+    //            console.log(number);
+    //        }
+    //    }
+         
+    
+    
+    
+    // daxil edilen 2 eded arasinda sonu 7 ile bitenleri tapn 
+
+        // let a = +prompt("Birinci ededi daxil edin");
+        // let b = +prompt("sonuncu ededi daxil edin");
+        
+        // console.log(`${a} ${b} arasinda sonu 7 ile biten ededler`);
+        // for (let i = a; i <= b; i++) {
+        //     if (i % 10 === 7) {
+        //         console.log(i);
+        //     }
+        // }
+
+
+
+
+
+
+
